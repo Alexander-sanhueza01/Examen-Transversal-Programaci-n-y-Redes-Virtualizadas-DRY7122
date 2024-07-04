83bdf1ee511fdaa24c32257571862692b97ae85c
Alexander-sanhueza01 <163456996+Alexander-sanhueza01@users.noreply.github.com>
2024-07-04 15:30:25 -0400

    Create Nombres.py

---
+++ Nombres.py
@@ -0,0 +1,7 @@
+# Definir la lista de nombres
+nombres = ["Alex Alvarado", "Nicolas Leiton", "Alexander Sanhueza"]
+
+# Imprimir la lista de nombres
+print("Lista de nombres:")
+for nombre in nombres:
+    print(nombre)
